8b95e18cec8c048224142358123bb78d594ff63d
Michael Budnick <mdbudnick@users.noreply.github.com>
2023-05-29 22:25:09 -0400

    Make team_coaches an EndpointDefinition

--- src/endpoints.ts
+++ src/endpoints.ts
@@ -23,7 +23,7 @@ export const ENDPOINTS: Record<string, EndpointDefinition> = { attendance, award
     game_color_timestamps, game_linescore, game_playByPlay, gamePace, highLow, homeRunDerby, league, league_allStarBallot, league_allStarWriteIns, league_allStarFinalVote,
     people, people_changes, people_freeAgents, person, person_stats, jobs, jobs_umpire_games, jobs_datacasters, jobs_officialScorers,
     schedule, schedule_postseason, schedule_postseason_series, schedule_postseason_tuneIn, schedule_tied, seasons, season, sports, sports_players, standings, stats, stats_leaders, stats_streaks,
-    team, team_alumni, teams, teams_history, teams_stats, teams_affiliates
+    team, team_alumni, team_coaches, teams, teams_history, teams_stats, teams_affiliates
 } 
 
 const attendance: EndpointDefinition = {
@@ -1108,7 +1108,7 @@ const team_alumni: EndpointDefinition = {
         "query_params": ["season", "group", "hydrate", "fields"],
         "required_params": [["season", "group"]],
     }
-    "team_coaches": {
+const team_coaches: EndpointDefinition = {
         "url": BASE_URL + "{ver}/teams/{teamId}/coaches",
         "path_params": {
             "ver": {
@@ -1128,7 +1128,7 @@ const team_alumni: EndpointDefinition = {
         },
         "query_params": ["season", "date", "fields"],
         "required_params": [[]],
-    },
+    }
     "team_personnel": {
         "url": BASE_URL + "{ver}/teams/{teamId}/personnel",
         "path_params": {
