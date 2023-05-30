58a5fea45c1fa462279c7442ce7bdbe8304de75e
Michael Budnick <mdbudnick@users.noreply.github.com>
2023-05-29 22:31:48 -0400

    Make transactions an EndpointDefinition

--- src/endpoints.ts
+++ src/endpoints.ts
@@ -23,7 +23,7 @@ export const ENDPOINTS: Record<string, EndpointDefinition> = { attendance, award
     game_color_timestamps, game_linescore, game_playByPlay, gamePace, highLow, homeRunDerby, league, league_allStarBallot, league_allStarWriteIns, league_allStarFinalVote,
     people, people_changes, people_freeAgents, person, person_stats, jobs, jobs_umpire_games, jobs_datacasters, jobs_officialScorers,
     schedule, schedule_postseason, schedule_postseason_series, schedule_postseason_tuneIn, schedule_tied, seasons, season, sports, sports_players, standings, stats, stats_leaders, stats_streaks,
-    team, team_alumni, team_coaches, team_leaders, team_personnel, team_roster, team_stats, teams, teams_history, teams_stats, teams_affiliates
+    team, team_alumni, team_coaches, team_leaders, team_personnel, team_roster, team_stats, teams, teams_history, teams_stats, teams_affiliates, transactions
 } 
 
 const attendance: EndpointDefinition = {
@@ -1306,7 +1306,7 @@ const teams_affiliates: EndpointDefinition = {
         "query_params": ["teamIds", "sportId", "season", "hydrate", "fields"],
         "required_params": [["teamIds"]],
     }
-    "transactions": {
+const transactions: EndpointDefinition = {
         "url": BASE_URL + "{ver}/transactions",
         "path_params": {
             "ver": {
@@ -1332,7 +1332,7 @@ const teams_affiliates: EndpointDefinition = {
             ["date"],
             ["startDate", "endDate"],
         ],
-    },
+    }
     "venue": {
         "url": BASE_URL + "{ver}/venues",
         "path_params": {
